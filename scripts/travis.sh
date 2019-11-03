015a2c3511fcea5ffe9baf90fe83b5dd552dd86d
Christian Berendt <berendt@betacloud-solutions.de>
2019-11-03 11:53:40 +0100

    Update from global travis repository

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce
+sudo apt-get update
+sudo apt-get -y install docker-ce
 echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 
-echo $TRAVIS_DOCKER_PASSWORD | docker login --username="$TRAVIS_DOCKER_USERNAME" --password-stdin
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo $TRAVIS_DOCKER_PASSWORD | docker login --username="$TRAVIS_DOCKER_USERNAME" --password-stdin
+fi
