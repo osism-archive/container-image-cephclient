99777d3fb39dd7c63c7ff3997ec3ccbcb2777009
Christian Berendt <berendt@osism.tech>
2021-07-11 12:07:54 +0200

    Push e.g. osism/cephclient:16.2.5
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/push.sh
+++ scripts/push.sh
@@ -14,6 +14,16 @@ if [[ -n $DOCKER_REGISTRY ]]; then
     REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
 fi
 
+# push e.g. osism/cephclient:pacific
 docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$VERSION"
 docker push "$REPOSITORY:$VERSION"
-docker rmi "$REPOSITORY:$VERSION"
+
+# push e.g. osism/cephclient:16.2.5
+version=$(docker run --rm "$REPOSITORY:$VERSION" ceph --version | awk '{ print $3 }')
+
+if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect "${REPOSITORY}:${version}" > /dev/null; then
+    echo "The image ${REPOSITORY}:${version} already exists."
+else
+    docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+    docker push "$REPOSITORY:$version"
+fi
